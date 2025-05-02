38912867ca990dbd1d07c7a1dbdc1ab0fc91ed54
SS <andyxvuitton@gmail.com>
2025-05-02 02:03:38 -0700

    modified:   about.html 	modified:   css/elementAEOC.css 	new file:   images/rhacDonation.png 	new file:   js/popup.js

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+// Function to show the popup
+function showPopup() {
+    document.getElementById('qrPopup').style.display = 'flex';
+}
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById('qrPopup').style.display = 'none';
+}
